d2eb0d183a77e2c82bdce7ed3b98c87aa32af75d
Zach-Robertson19 <zachrobertson19@gmail.com>
2021-09-29 00:15:12 -0500

    Move away from rss feed and use /tx/txid url instead

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # BitPost Analytics
 
+```bash
+__________.__  __ __________               __
+\______   \__|/  |\______   \____  _______/  |_
+ |    |  _/  \   __\     ___/  _ \/  ___/\   __\
+ |    |   \  ||  | |    |  (  <_> )___ \  |  |
+ |______  /__||__| |____|   \____/____  > |__|
+        \/                            \/
+   _____                .__           __  .__
+  /  _  \   ____ _____  |  | ___.__._/  |_|__| ____   ______
+ /  /_\  \ /    \\__  \ |  |<   |  |\   __\  |/ ___\ /  ___/
+/    |    \   |  \/ __ \|  |_\___  | |  | |  \  \___ \___ \
+\____|__  /___|  (____  /____/ ____| |__| |__|\___  >____  >
+        \/     \/     \/     \/                   \/     \/
+```
+
 This is a program to help gather analytics from the BitPost RSS feed. Later it will be updated to allow for easy scraping of the RSS feed with tools to help with statistical analysis and plotting.
 
 ## How to use
 
-Right now there is only one file, `main.py`, you use it by entering names of users you want to scrape the RSS feed of into the `users.txt` file then running the `main.py` file. This will create three different plots, one for the average number of words per post for each user, the other for the total number of images in all articles for each user and the last is the relationship between these two values for each user.
-*This is just a very quick program I threw together for day 15 of the BitPost 30 day challenge, it will be updated in the future to do more stuff*
+Right now there is only one file, `main.py`, you use it by entering names of users you want to scrape the RSS feed of into the `users.txt` file then running the `main.py` file. This will create a few different graphs visualizing the statistics of the BitPost articles from each users in `users.txt`.
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-import bs4
+import time
+import json
+import random
 import requests
+import datetime
+import pyfiglet
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
@@ -9,104 +12,153 @@ from bs4 import BeautifulSoup
 
 class BitPost:
     def __init__(self):
+        custom_fig = pyfiglet.Figlet(font='graffiti')
+        ascii_banner = custom_fig.renderText('BitPost Analytics')
+        print(ascii_banner)
+
         self.base_url = 'https://bitpost.app'
         self.data = pd.DataFrame(columns=['author', 'title', 'nw', 'noi'])
-        self.color_list = list(mcolors.CSS4_COLORS.items())
+        self.color_list = list(mcolors.TABLEAU_COLORS.items())
         self.users = self._read_user_file('users.txt')
+
+        sep_start = datetime.date(2021, 9, 1)
+        self.start_unix_time = time.mktime(sep_start.timetuple())
 
     def _read_user_file(self, filename):
         f = open(filename, 'r')
         users = f.read().split('\n')
-        users = [item.replace('\'', '').replace(',', '') for item in users[1:]]
+        users = [item for item in users[1:]]
         return users
 
-    def _get_user_rss(self, user_name):
-        print(f'------------------Making a request for the rss page of {user_name}------------------')
-        user_rss_url = f'{self.base_url}/u/{user_name}/rss'
-        rss_response = requests.get(user_rss_url)
+    def _query_profie_page(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, features='html.parser')
+        for link in soup.findAll(class_='link dark-gray hover-hot-pink'):
+            print(link.attrs)
+        next_page = soup.findAll(class_='db w2 h2 link mid-gray br3 pointer | pagination-link')
+        if len(next_page) > 0:
+            print(next_page)
+            link = next_page[0].attrs['href']
+            self._query_profie_page(link)
+
+    def _get_post_data_from_tx(self, tx):
+        response = requests.get(f'{self.base_url}/tx/{tx}')
+        soup = BeautifulSoup(response.content, features='html.parser')
+
+        author = soup.findAll(class_='link fw6 mid-gray hover-hot-pink')
+        assert len(author) == 1
+        author = author[0].text
+
+        title = soup.findAll(class_='flex-auto mv0 pr3 f1 lh-title')
+        assert len(title) == 1
+        title = title[0].text
+
+        content = soup.findAll(class_='content-wrap')
         
-        soup = BeautifulSoup(rss_response.content, features='html.parser')
+        if len(content) != 1:
+            return author, title, 0, 0
 
-        # Loop through each individual article
-        for article in soup.findAll('item'):
-            # Skip posts that are not articles
-            if article.description is None:
-                continue
+        nw = 0
+        for child in content[0].findChildren():
+            if child.name == 'p':
+                nw += len(child.text.split())
+        
+        noi = self._get_noi_from_soup(content[0])
 
-            # Get the content form the article
-            content = article.description.findAll(text=True)
-            nw = 0
-            noi = 0
-            for item in content:
-                if isinstance(item, bs4.element.NavigableString):
-                    paragraphs = BeautifulSoup(item, features='html.parser').findAll('p')
-                    for paragraph in paragraphs:                        
-                        # Clean the contents of the paragraph and count the total number of words
-                        for item in paragraph:
-                            if isinstance(item, bs4.element.NavigableString):
-                                words = re.findall(r'(\w+)', item)
-                                nw += len(words)
+        return author, title, nw, noi
 
-                        # Get the number of images in the paragraph
-                        imgs = paragraph.findAll('img')
-                        if imgs is not None:
-                            noi += len(imgs)
+    def _get_all_data_for_user(self, user):
+        print(
+            f'Getting data for {user}'
+        )
+        url = f'{self.base_url}/u/{user}/bitfeed'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, features='html.parser')
+        data = json.loads(soup.text)
 
-            self.data = self.data.append(
-                { 
-                    'author' : user_name, 
-                    'title' : article.title.contents[0], 
-                    'nw' : nw,
-                    'noi' : noi
-                }, 
-                ignore_index=True
-            )
-    
-    def _get_all_user_rss(self):
+        titles = []
+        for item in data['tx']:
+            if item['timestamp'] < self.start_unix_time:
+                break
+            tx = item['id']
+            author, title, nw, noi = self._get_post_data_from_tx(tx)
+            if title not in titles:
+                print(
+                    f'Saving data for {title} by {user}'
+                )
+                titles.append(title)
+                self.data = self.data.append(
+                    {
+                        'author' : author,
+                        'title' : title,
+                        'nw' : nw,
+                        'noi' : noi
+                    }, ignore_index=True)
+
+    def _get_noi_from_soup(self, soup):
+        return len(soup.findAll('img'))
+
+    def _get_all_data(self):
         # Make sure there are not duplicate users
         assert len(self.users) == len(set(self.users))
 
-        self.colors = {}
-        for index, user in enumerate(self.users):
-            self.colors.update({ user : self.color_list[index % len(self.color_list)][0]})
-            self._get_user_rss(user)
+        self.colors = []
+        for _, user in enumerate(self.users):
+            color = random.choice(self.color_list)[0]
+            self.colors.append(color)
+            self._get_all_data_for_user(user)
+
+    def _save_data(self):
+        print(
+            'Saving dataframe to data.csv'
+        )
+        self.data.to_csv('data.csv', index=False)
 
     def _run_statistics_on_all_users(self):
-        users = set(self.data['author'].tolist())
+        users = list(set(self.data['author'].tolist()))
         avg_nw = []
         avg_noi = []
+        tot_nw = []
         tot_noi = []
         for item in users:
             user_nw_list = self.data[self.data['author'] == item]['nw'].tolist()
             user_noi_list = self.data[self.data['author'] == item]['noi'].tolist()
 
             user_avg_nw = int(sum(user_nw_list) / len(user_nw_list))
+            user_tot_nw = sum(user_nw_list)
             
             user_tot_noi = sum(user_noi_list)
-            user_avg_noi = int(user_tot_noi / len(user_noi_list))
+            user_avg_noi = (user_tot_noi / len(user_noi_list))
 
             avg_nw.append(user_avg_nw)
             avg_noi.append(user_avg_noi)
+            tot_nw.append(user_tot_nw)
             tot_noi.append(user_tot_noi)
-        return avg_nw, avg_noi, tot_noi
+        return users, avg_nw, avg_noi, tot_nw, tot_noi
 
-
-    def _plot_data(self, x, y, x_label, y_label):
+    def _plot_data(self, x, y, x_label, y_label, annotate=None):
         plt.scatter(
-            x, y, c = set(self.data['author'].map(self.colors).tolist())
+            x, y, c = self.colors
         )
         plt.xlabel(x_label)
         plt.ylabel(y_label)
+        plt.xticks(rotation=90)
+        plt.subplots_adjust(bottom=0.25)
+        plt.grid()
+        if annotate is not None:
+            for i, user in enumerate(annotate):
+                plt.annotate(user, (x[i], y[i]))
         plt.show()
 
     def run(self):
-        self._get_all_user_rss()
-        avg_nw, avg_noi, tot_noi = self._run_statistics_on_all_users()
-        self._plot_data(range(len(avg_nw)), avg_nw, 'User', 'Average number of words per article')
-        self._plot_data(range(len(tot_noi)), tot_noi, 'User', 'Total number of images in all articles')
-        self._plot_data(range(len(avg_noi)), avg_noi, 'Users', 'Average number of images in each article')
-        self._plot_data(tot_noi, avg_nw, 'Total number of images in all articles', 'Average number of words per article')
-        
+        self._get_all_data()
+        self._save_data()
+        users, avg_nw, avg_noi, tot_nw, tot_noi = self._run_statistics_on_all_users()
+        self._plot_data(users, avg_nw, 'User', 'Average number of words per article')
+        self._plot_data(users, avg_noi, 'Users', 'Average number of images in each article')
+        self._plot_data(users, tot_nw, 'User', 'Total number of words in all articles')
+        self._plot_data(users, tot_noi, 'User', 'Total number of images in all articles')
+        self._plot_data(tot_nw, tot_noi, 'Total number of words', 'Total number of images', annotate=users)
 
 if __name__ == "__main__":
     bp = BitPost()
